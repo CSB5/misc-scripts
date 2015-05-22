491b93f42b57a6987f6c702f11eb29c02a52829a
Andreas WILM (GIS) <wilma@gis.a-star.edu.sg>
2015-05-22 11:43:17 +0800

    coverage_from_bam.sh: - checking if arguments where given and files exist - splitting bed into regions for faster pileup (random access)

--- coverage_from_bam.sh
+++ coverage_from_bam.sh
@@ -2,12 +2,16 @@
 
 BED=$1
 BAM=$2
+test -z "$BED" && exit 1
+test -z "$BAM" && exit 1
+test -e "$BED" || exit 1
+test -e "$BAM" || exit 1
 
 # Get the length from the bed file
 BED_LEN=$(awk '{if (/^[^#]/) {d[$1]+=($3-$2)}} END {for (c in d) {s+=d[c]}; print  s}' $BED)
 
 # Get the total coverage from the bam file
-BAM_COV=$(samtools mpileup -d 100000 -Q 5 -l $BED $BAM | cut -f 4 | datamash sum 1)
+BAM_COV=$(grep -v '^#' $BED | while read c s e _; do let s=s+1; samtools mpileup -d 100000 -Q 5 -r "$c:$s-$e" $BAM | cut -f 4; done | datamash sum 1)
 test -z "$BAM_COV" && BAM_COV=0
 
 echo "BED length: $BED_LEN"
